62cdfdff01fea640473cca6ed7ef71abcd4f3f21
Andrew Makris <amakri386@west-mec.org>
2023-06-17 13:57:09 -0700

    Deleted bad idea

--- jl/src/Pages/Movies.js
+++ jl/src/Pages/Movies.js
@@ -3,16 +3,7 @@ import React from 'react'
 const Movies = () => {
   return (
     <div className="movie-container">
-        <h2>JUSTICE LEAGUE IN MOVIES</h2>
-        {/* <hr/> */}
-        <div className="movie-flex">
-            <div className="anime">
-                <h4 className="anime-text">Animated Movies</h4>
-            </div>
-            <div className="live">
-                <h4 className='live-text'>Live Action Movies</h4>
-            </div>
-        </div>
+        
     </div>
   )
 }
