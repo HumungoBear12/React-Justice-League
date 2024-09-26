399eb9419b108b1ec1f35dcdcbb9ab97d4f198a3
Andrew Makris <amakris03@gmail.com>
2024-09-26 11:00:21 -0700

    push

--- jl/src/Pages/Superman.js
+++ jl/src/Pages/Superman.js
@@ -2,33 +2,36 @@ import React from 'react'
 
 const Superman = () => {
   return (
-    <div className='char-container'>
-      <div className='one'>
-        <h2><strong>Superman: The Man of Steel</strong></h2><hr/>
-        <div className='three'>
-          <p>
-            Superman, created by Jerry Siegel and Joe Shuster, debuted in Action Comics #1 in 1938 and is one of the most iconic superheroes. He hails from the planet Krypton, where he was born as Kal-El. His parents, Jor-El and Lara, sent him to Earth as a baby when Krypton was about to be destroyed. He was found and raised by Jonathan and Martha Kent in Smallville, Kansas, and took on the name Clark Kent. As he grew up, Clark discovered he had superhuman abilities due to Earth’s yellow sun, such as super strength, speed, flight, x-ray vision, and heat vision. He adopts the superhero identity of Superman and fights for truth, justice, and peace.
-            <br/>
-            <br/>
-            <br/>
-            Superman’s main adversaries include Lex Luthor, a brilliant and wealthy scientist who despises Superman’s power and influence, and General Zod, a fellow Kryptonian with similar powers but a tyrannical mindset. Other notable villains include Brainiac, an alien android obsessed with collecting knowledge, and Doomsday, the monstrous being who famously killed Superman in the 1990s comic storyline, The Death of Superman.
-            <br/>
-            <br/>
-            <br/>
-            Fun Facts:<br/>
-            - Superman was the first superhero to wear a cape.<br/>
-            - His "S" symbol originally stood for Superman, but later it was redefined as a Kryptonian symbol for hope.<br/>
-            - Superman’s alter ego, Clark Kent, works as a reporter for the Daily Planet.<br/>
-            - He was briefly portrayed as powerless in the Superman: Red Son storyline, where he was raised in the Soviet Union instead of the U.S.<br/>
-            - Superman’s weakness is kryptonite, a mineral from his home planet.<br/>
-            </p>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/vExdZjmWhhI?si=CFurtqYJHbnqvvCf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+   <div>
+     <h2 className="title-character"><strong>Superman: The Man of Steel</strong></h2><hr className="hr4"/>
+      <div className='char-container'>
+        <div className='one'>
+          <div className='three'>
+            <h2>Character Description</h2><hr/>
+            <p>
+              Superman, created by Jerry Siegel and Joe Shuster, debuted in Action Comics #1 in 1938 and is one of the most iconic superheroes. He hails from the planet Krypton, where he was born as Kal-El. His parents, Jor-El and Lara, sent him to Earth as a baby when Krypton was about to be destroyed. He was found and raised by Jonathan and Martha Kent in Smallville, Kansas, and took on the name Clark Kent. As he grew up, Clark discovered he had superhuman abilities due to Earth’s yellow sun, such as super strength, speed, flight, x-ray vision, and heat vision. He adopts the superhero identity of Superman and fights for truth, justice, and peace.
+              <br/>
+              <br/>
+              <br/>
+              Superman’s main adversaries include Lex Luthor, a brilliant and wealthy scientist who despises Superman’s power and influence, and General Zod, a fellow Kryptonian with similar powers but a tyrannical mindset. Other notable villains include Brainiac, an alien android obsessed with collecting knowledge, and Doomsday, the monstrous being who famously killed Superman in the 1990s comic storyline, The Death of Superman.
+              <br/>
+              <br/>
+              <br/>
+              Fun Facts:<br/>
+              - Superman was the first superhero to wear a cape.<br/>
+              - His "S" symbol originally stood for Superman, but later it was redefined as a Kryptonian symbol for hope.<br/>
+              - Superman’s alter ego, Clark Kent, works as a reporter for the Daily Planet.<br/>
+              - He was briefly portrayed as powerless in the Superman: Red Son storyline, where he was raised in the Soviet Union instead of the U.S.<br/>
+              - Superman’s weakness is kryptonite, a mineral from his home planet.<br/>
+              </p>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/vExdZjmWhhI?si=CFurtqYJHbnqvvCf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+        </div>
+        <div className='two'>
+          <img src="https://i.pinimg.com/564x/f9/d8/75/f9d8756d49f21ade2ce285d1bb77c5c4.jpg" alt="" />
         </div>
       </div>
-      <div className='two'>
-        <img src="https://i.pinimg.com/564x/f9/d8/75/f9d8756d49f21ade2ce285d1bb77c5c4.jpg" alt="" />
-      </div>
-    </div>
+   </div>
   )
 }
 
